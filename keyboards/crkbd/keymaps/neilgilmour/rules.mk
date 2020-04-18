38297cf55950b76de936c09674104d2f72685150
neil <neil@motional.io>
2020-04-18 23:06:44 +0100

    Enable RGB

--- keyboards/crkbd/keymaps/neilgilmour/rules.mk
+++ keyboards/crkbd/keymaps/neilgilmour/rules.mk
@@ -1,4 +1,4 @@
-RGBLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/glcdfont.c \
@@ -7,7 +7,11 @@ SRC +=  ./lib/glcdfont.c \
         ./lib/logo_reader.c \
         ./lib/keylogger.c \
         ./lib/mode_icon_reader.c \
-        ./lib/host_led_state_reader.c \
-        ./lib/timelogger.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
 
 EXTRAFLAGS += -flto
+OLED_DRIVER_ENABLE = yes
+
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
